e303d485d4d708fe1d2a71c147b941730a2dbb17
Tiago <tiagoalvespedrosa@gmail.com>
2015-06-17 00:27:09 -0400

    Start the next release cycle

--- src/main/java/Sholl_Analysis.java
+++ src/main/java/Sholl_Analysis.java
@@ -74,7 +74,7 @@ public class Sholl_Analysis implements PlugIn, DialogListener {
 
 	/* Plugin Information */
 	/** The Plugin's version */
-	public static final String VERSION = "3.4.5";
+	public static final String VERSION = "3.4.6-DEV";
 	private static final String URL = "http://fiji.sc/Sholl";
 
 	/* Sholl Type Definitions */
