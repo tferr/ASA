9ceae79ff9423f0db68ffcc25736dc4b9f0656e0
Tiago <tiagoalvespedrosa@gmail.com>
2016-05-17 09:17:18 -0400

    Bump IJ1 requirements for non-Fiji users.

--- src/main/java/sholl/Sholl_Analysis.java
+++ src/main/java/sholl/Sholl_Analysis.java
@@ -234,7 +234,7 @@ public class Sholl_Analysis implements PlugIn, DialogListener {
 	@Override
 	public void run( final String arg) {
 
-		if (IJ.versionLessThan("1.50e")) return; // this is required for non-fiji users
+		if (IJ.versionLessThan("1.51a")) return; // this is required for non-fiji users
 
 		if (arg.equalsIgnoreCase("sample")) {
 			img = Sholl_Utils.displaySample();
