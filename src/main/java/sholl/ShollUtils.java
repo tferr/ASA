20c1f03e85588f992c2b538bf57fb07c032fac46
Tiago <tiagoalvespedrosa@gmail.com>
2017-07-02 00:06:19 -0400

    Add method to retrieve sample data

--- src/main/java/sholl/ShollUtils.java
+++ src/main/java/sholl/ShollUtils.java
@@ -1,9 +1,15 @@
 package sholl;
 
+import java.io.IOException;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.PrimitiveIterator.OfInt;
 import java.util.stream.IntStream;
+
+import net.imagej.table.ResultsTable;
+import net.imagej.table.TableLoader;
 
 public class ShollUtils {
 
@@ -12,6 +18,37 @@ public class ShollUtils {
 
 	public static String d2s(final double d) {
 		return new DecimalFormat("#.###").format(d);
+	}
+
+	// this method is from BAR
+	public static URL getResource(final String resourcePath) {
+		final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		URL resource = null;
+		try {
+			final Enumeration<URL> resources = loader.getResources(resourcePath);
+			while (resources.hasMoreElements()) {
+				resource = resources.nextElement();
+				if (resource.toString().contains(resourcePath))
+					return resource;
+			}
+		} catch (final IOException exc) {
+			// proceed with return null;
+		}
+		return resource;
+	}
+
+	protected static ResultsTable csvSample() {
+		final URL url = getResource("csv/ddaCsample.csv");
+		if (url == null)
+			throw new NullPointerException("Could not retrieve ddaCsample.csv");
+		final TableLoader loader = new TableLoader();
+		try {
+			// NB: this will fail for headings containing whitespace[
+			return loader.valuesFromTextFile(url);
+		} catch (final IOException exc) {
+			exc.printStackTrace();
+			return null;
+		}
 	}
 
 	public static ArrayList<Double> getRadii(final double startRadius, final double incStep, final double endRadius) {
