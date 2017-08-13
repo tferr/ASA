dc68c31443cba66d79e7e1f0403dcf0a535e05d5
Tiago <tiagoalvespedrosa@gmail.com>
2017-08-13 15:38:48 -0400

    Reinstate gui.Utils as it is still used by SNT

---
+++ src/main/java/sholl/gui/Utils.java
@@ -0,0 +1,72 @@
+/*
+ * #%L
+ * Sholl_Analysis plugin for ImageJ
+ * %%
+ * Copyright (C) 2016 Tiago Ferreira
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package sholl.gui;
+
+import java.awt.Color;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JMenuItem;
+
+@Deprecated
+public class Utils {
+
+	private Utils() {
+	}
+
+	@Deprecated
+	public static final void improveRecording() {
+		EnhancedGenericDialog.improveRecording();
+	}
+
+	@Deprecated
+	public static Color getDisabledComponentColor() {
+		return EnhancedGenericDialog.getDisabledComponentColor();
+	}
+
+	@Deprecated
+	public static JMenuItem menuItemTrigerringURL(final String label, final String URL) {
+		return EnhancedGenericDialog.menuItemTrigerringURL(label, URL);
+	}
+
+	@Deprecated
+	public static JMenuItem menuItemTriggeringResources() {
+		return EnhancedGenericDialog.menuItemTriggeringResources();
+	}
+
+	/**
+	 * @return {@code true} if running on an headless environment
+	 */
+	public static boolean isHeadless() {
+		return GraphicsEnvironment.isHeadless();
+	}
+
+	@Deprecated
+	public final static Color infoColor() {
+		return EnhancedGenericDialog.infoColor();
+	}
+
+	@Deprecated
+	public final static Color warningColor() {
+		return EnhancedGenericDialog.warningColor();
+	}
+
+}
